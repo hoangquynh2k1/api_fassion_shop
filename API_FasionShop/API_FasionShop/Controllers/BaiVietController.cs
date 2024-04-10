3568bd6e3140518bdc88206a7f2d2e1eb4809772
hoangquynh2k1 <hoangquynh12a3dqg@gmail.com>
2024-04-10 21:33:31 +0700

    them controller bai viet

--- API_FasionShop/API_FasionShop/Controllers/BaiVietController.cs
+++ API_FasionShop/API_FasionShop/Controllers/BaiVietController.cs
@@ -1,4 +1,5 @@
 ﻿using API_FashionShop.BUS;
+using API_FashionShop.Entities;
 using API_FashionShop.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,9 +16,59 @@ namespace API_FashionShop.Controllers
             baiVietBUS = new BaiVietBUS(db);
         }
         [HttpGet]
-        public List<BaiViet> GetAll()
+        public Respone GetAll()
         {
-            return baiVietBUS.Gets();
+            return new Respone(true, Status.Success, string.Empty, baiVietBUS.Gets());
+        }
+        [HttpGet("{id}")]
+        public Respone GetById(int id)
+        {
+            return new Respone(true, Status.Success, string.Empty, baiVietBUS.Get(id));
+        }
+        [HttpPost]
+        public Respone Create(BaiViet o)
+        {
+            try
+            {
+                if (o.TieuDe == string.Empty || o.NoiDung == string.Empty)
+                    return new Respone(false, Status.BadRequest, string.Empty);
+                var result = baiVietBUS.Create(o);
+                return new Respone(true, Status.Success, string.Empty, o);
+            }
+            catch (Exception ex)
+            {
+                return new Respone(false, Status.ApplicationError, string.Empty);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public Respone Upfate(int id, BaiViet o)
+        {
+            try
+            {
+                if (o.TieuDe == string.Empty || o.NoiDung == string.Empty)
+                    return new Respone(false, Status.BadRequest, string.Empty);
+                var result = baiVietBUS.Update(o);
+                return new Respone(true, Status.Success, string.Empty, result);
+            }
+            catch (Exception ex)
+            {
+                return new Respone(false, Status.ApplicationError, string.Empty);
+            }
+        }
+
+        [HttpDelete]
+        public Respone Delete(int id)
+        {
+            try
+            {
+                var result = baiVietBUS.Delete(id);
+                return new Respone(true, Status.Success, string.Empty, result);
+            }
+            catch (Exception ex)
+            {
+                return new Respone(false, Status.ApplicationError, string.Empty);
+            }
         }
     }
 }
--- API_FasionShop/API_FasionShop/DAO/BaiVietDAO.cs
+++ API_FasionShop/API_FasionShop/DAO/BaiVietDAO.cs
@@ -11,7 +11,44 @@ namespace API_FashionShop.DAO
         }
         public List<BaiViet> Gets()
         {
-            return db.BaiViets.ToList();
+            return db.BaiViets.Where(x => x.TrangThai == true).ToList();
+        }
+        public BaiViet Get(int id)
+        {
+            return db.BaiViets.Where(x => x.TrangThai == true).First(x => x.Id == id);
+        }
+        public bool Create(BaiViet o)
+        {
+            var result = db.BaiViets.Add(o);
+            if(result != null)
+            {
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        public bool Update(BaiViet o)
+        {
+            var result = db.BaiViets.Where(x => x.TrangThai == true).First(x => x.Id == o.Id);
+            if (result != null)
+            {
+                result.TieuDe = o.TieuDe;
+                result.NoiDung = o.NoiDung;
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        public bool Delete(int id)
+        {
+            var result = db.BaiViets.Where(x => x.TrangThai == true).First(x => x.Id == id);
+            if (result != null)
+            {
+                result.TrangThai = false;
+                db.SaveChanges();
+                return true;
+            }
+            return false;
         }
     }
 }
