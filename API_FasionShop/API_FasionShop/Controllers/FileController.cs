24afb64d4e21cbf990784ee64e322b4cf62a76a5
hoangquynh2k1 <hoangquynh12a3dqg@gmail.com>
2024-05-06 16:19:45 +0700

    add pd logic

---
+++ API_FasionShop/API_FasionShop/Controllers/FileController.cs
@@ -0,0 +1,57 @@
+﻿using API_FashionShop.Entities;
+using API_FashionShop.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_FashionShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        FileService fileService = new FileService();
+
+        [HttpPost("Upload/Avatar")]
+        public async Task<Respone> UploadAvartar(IFormFile f)
+        {
+            try
+            {
+                var result = await fileService.Upload(f, "Avatars");
+                return new Respone(true, Status.Success, string.Empty, result);
+            }
+            catch (Exception ex)
+            {
+                return new Respone(false, Status.ApplicationError, string.Empty, ex.Message);
+            }
+        }
+
+        [HttpPost("Upload/Product")]
+        public async Task<Respone> UploadProduct(IFormFile f)
+        {
+            try
+            {
+                var result = await fileService.Upload(f, "Products");
+                return new Respone(true, Status.Success, string.Empty, result);
+            }
+            catch (Exception ex)
+            {
+                return new Respone(false, Status.ApplicationError, string.Empty, ex.Message);
+            }
+        }
+
+        [HttpGet("ReadFile")]
+        public ActionResult ReadFile(string path)
+        {
+            try
+            {
+                var result = fileService.Read(path);
+                return File(result, "image/jpeg");
+                //return new Respone(true, Status.Success, string.Empty, a);
+            }
+            catch (Exception ex)
+            {
+                return Ok(null);
+            }
+        }
+    }
+}
--- API_FasionShop/API_FasionShop/Controllers/KhachHangController.cs
+++ API_FasionShop/API_FasionShop/Controllers/KhachHangController.cs
@@ -118,49 +118,5 @@ namespace API_FashionShop.Controllers
                 return new Respone(false, Status.ApplicationError, string.Empty, ex.Message);
             }
         }
-
-        [HttpPost]
-        public async Task<Respone> Upload(IFormFile f)
-        {
-            try
-            {
-                var result = await fileService.Upload(f, "Avatars");
-                return new Respone(true, Status.Success, string.Empty, result);
-            }
-            catch (Exception ex)
-            {
-                return new Respone(false, Status.ApplicationError, string.Empty, ex.Message);
-            }
-        }
-
-        //[HttpGet]
-        //public Respone ReadFile(string path)
-        //{
-        //    try
-        //    {
-        //        var result = fileService.Read(path);
-        //        var a = File(result, "image/jpeg");
-        //        return new Respone(true, Status.Success, string.Empty, a);
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        return new Respone(false, Status.ApplicationError, string.Empty, ex.Message);
-        //    }
-        //}
-        [HttpGet]
-        public ActionResult ReadFile(string path)
-        {
-            try
-            {
-                var result = fileService.Read(path);
-                return File(result, "image/jpeg");
-                //return new Respone(true, Status.Success, string.Empty, a);
-            }
-            catch (Exception ex)
-            {
-                //return new Respone(false, Status.ApplicationError, string.Empty, ex.Message);
-                return null;
-            }
-        }
     }
 }
